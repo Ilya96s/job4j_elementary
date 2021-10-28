9c27464401118e1822c469d555394a561b305757
Ilya96s <19961204172014_Ilya>
2021-10-28 20:38:07 +0700

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -5,10 +5,9 @@ public class Multiple {
         System.out.println("1*2=2");
         System.out.println("1*3=3");
         System.out.println("1*9=9");
-        // Вношу изменения
         System.out.println("1*10=10");
-        // Дополнительные изменения
         System.out.println("2*2=4");
         System.out.println("10*10=100");
+        System.out.println("2*5=10");
     }
 }
