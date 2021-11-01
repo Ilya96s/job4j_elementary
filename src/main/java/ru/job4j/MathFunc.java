39a4629d2ab00c7ffe16a67f0ff8971ba4a21f7c
Ilya96s <19961204172014_Ilya>
2021-11-01 22:06:12 +0700

    4.2. Результат работы метода [#232622]

--- src/main/java/ru/job4j/MathFunc.java
+++ src/main/java/ru/job4j/MathFunc.java
@@ -1,7 +1,6 @@
 package ru.job4j;
 
 public class MathFunc {
-
     public static int func1(int x) {
         int y = x * x + 1;
         return y;
@@ -20,4 +19,3 @@ public class MathFunc {
         System.out.println(total);
     }
 }
-
