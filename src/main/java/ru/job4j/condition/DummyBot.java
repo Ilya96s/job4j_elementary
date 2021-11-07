f2d27a6a214a89c79aa84247a0bfb3b94a2cc657
Ilya96s <19961204172014_Ilya>
2021-11-07 18:31:07 +0700

    4.1.2. Глупый бот [#31323]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -4,10 +4,10 @@ public class DummyBot {
     public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
-            rsl = "Привет, умник";
+            rsl = "Привет, умник.";
         }
         if ("Пока.".equals(question)) {
-            rsl = "До скорой встречи";
+            rsl = "До скорой встречи.";
         }
         return rsl;
     }
