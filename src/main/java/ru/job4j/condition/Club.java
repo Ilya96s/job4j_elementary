e282ce2dc47ac0b34127da6ec893b3971ff20a99
Ilya96s <i.kaltygin@icloud.com>
2021-11-11 18:12:11 +0300

    4.3.2. Оператор || [#333784 #226966]

--- src/main/java/ru/job4j/condition/Club.java
+++ src/main/java/ru/job4j/condition/Club.java
@@ -2,8 +2,7 @@ package ru.job4j.condition;
 
 public class Club {
     public static void permission(boolean hasMoney, boolean beFriend) {
-        boolean rsl = hasMoney || beFriend;
-        if (rsl) {
+        if (hasMoney || beFriend) {
             System.out.println("I can go to the club");
         } else {
             System.out.println("I can't");
