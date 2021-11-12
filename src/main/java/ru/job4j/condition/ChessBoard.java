81542de78bf2304331187beddb59f3b4c054b7e1
Ilya96s <i.kaltygin@icloud.com>
2021-11-12 22:31:04 +0300

    4.3.6. Math.abs [#373616 #227425]

---
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,38 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2) {
+     int rsl = 0;
+     if (isPositiveAndLessThan7(x1, y1, x2, y2) && isDiagonal(x1, y1, x2, y2)) {
+      rsl = Math.abs(x2 - x1);
+      rsl = rsl == Math.abs(y2 - y1) ? Math.abs(y2 - y1) : rsl;
+      }
+      return rsl;
+  }
+
+    public static boolean isPositive(int num1, int num2, int num3, int num4) {
+        return num1 >= 0 && num2 >= 0 && num3 >= 0 && num4 >= 0;
+    }
+
+    public static boolean isLessThan7(int num1, int num2, int num3, int num4) {
+        return num1 <= 7 && num2 <= 7 && num3 <= 7 && num4 <= 7;
+    }
+
+    public static boolean isPositiveAndLessThan7(int num1, int num2, int num3, int num4) {
+        return num1 >= 0 && num1 <= 7 && num2 >= 0 && num2 <= 7 && num3 >= 0 && num3 <= 7 && num4 >= 0 && num4 <= 7;
+    }
+
+    public static boolean isDiagonal(int x1, int y1, int x2, int y2) {
+        int first = Math.abs(x2 - x1);
+        int second = Math.abs(y2 - y1);
+        return first == second;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPositive(1, 0, 3, 2));
+        System.out.println(isLessThan7(1, 0, 3, 2));
+        System.out.println(isPositiveAndLessThan7(1, 0, 3, 2));
+        System.out.println(isDiagonal(1, 0, 3, 2));
+        System.out.println(way(1, 0, 3, 2));
+    }
+}
