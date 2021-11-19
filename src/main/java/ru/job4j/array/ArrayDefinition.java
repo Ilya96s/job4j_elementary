4036616d12a6948e2f54198e8ca9353f127f2bc3
Ilya96s <i.kaltygin@icloud.com>
2021-11-19 19:34:18 +0300

    6.0.1.1. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,7 +1,12 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
-    short[] ages = new short[10];
-    String[] surnames = new String[100500];
-    float[] prices = new float[40];
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+        System.out.println("Размер массива павен: " + ages.length);
+        System.out.println("Размер массива павен: " + surnames.length);
+        System.out.println("Размер массива павен: " + prices.length);
+    }
 }
