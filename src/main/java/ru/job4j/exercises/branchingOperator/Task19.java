624c11c4b410f52ff13416374af16aa5baa7fe4d
Ilya <i.kaltygin@icloud.com>
2022-12-20 21:53:30 +0700

    20. Сравнить два числа и выполнить действие

---
+++ src/main/java/ru/job4j/exercises/branchingOperator/Task19.java
@@ -0,0 +1,22 @@
+package ru.job4j.exercises.branchingOperator;
+
+/**
+ * Ниже представлен метод sumOrMultiply(int number1, int number2), который принимает 2 целых числа.
+ *
+ * Задание: Написать код, который выводит на экран сумму этих чисел, если первое больше второго, если же наоборот - выводит их произведение.
+ *
+ * В случае, если числа одинаковы, вывести сообщение "Числа равны".
+ *
+ * @author Ilya Kaltygin
+ */
+public class Task19 {
+    public static void sumOrMultiply(int number1, int number2) {
+        if (number1 > number2) {
+            System.out.println(number1 + number2);
+        } else if (number1 < number2) {
+            System.out.println(number1 * number2);
+        } else {
+            System.out.println("Числа равны");
+        }
+    }
+}
