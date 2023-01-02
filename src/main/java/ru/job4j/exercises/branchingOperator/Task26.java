80b99c3311945c162d6b1b9b6f44a19cdcbc573c
Ilya <i.kaltygin@icloud.com>
2023-01-02 23:39:54 +0700

    26. Равенство двух чисел

---
+++ src/main/java/ru/job4j/exercises/branchingOperator/Task26.java
@@ -0,0 +1,14 @@
+package ru.job4j.exercises.branchingOperator;
+
+/**
+ * Ниже представлен метод isEqual(int num1, int num2), который принимает 2 целых числа
+ *
+ * Задание: Написать код, который выводит в консоль "Да", если числа равны между собой, иначе "Нет".
+ *
+ * @author Ilya Kaltygin
+ */
+public class Task26 {
+    public static void isEqual(int num1, int num2) {
+        System.out.println(num1 == num2 ? "Да": "Нет");
+    }
+}
