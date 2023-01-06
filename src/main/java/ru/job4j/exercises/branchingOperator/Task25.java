f80fc2d00caaf8c7c8406649fe2ec85136974971
Ilya <i.kaltygin@icloud.com>
2023-01-06 21:19:56 +0700

    25. Положительное или отрицательное число

---
+++ src/main/java/ru/job4j/exercises/branchingOperator/Task25.java
@@ -0,0 +1,14 @@
+package ru.job4j.exercises.branchingOperator;
+
+/**
+ * Ниже представлен метод isPositive(int number), который принимает целое число.
+ *
+ * Задание: Написать код, который выводит в консоль "Да", если число положительное, иначе "Нет".
+ *
+ * @author Ilya Kaltygin
+ */
+public class Task25 {
+        public static void isPositive(int number) {
+            System.out.println(number >= 0 ? "Да" : "Нет");
+        }
+}
