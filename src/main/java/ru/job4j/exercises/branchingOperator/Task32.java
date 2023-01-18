644950283e37fcc268197241a16d33665f69e515
Ilya <i.kaltygin@icloud.com>
2023-01-18 21:56:14 +0700

    31. Вывод разницы или произведения двух чисел в зависимости от условия

---
+++ src/main/java/ru/job4j/exercises/branchingOperator/Task32.java
@@ -0,0 +1,24 @@
+package ru.job4j.exercises.branchingOperator;
+
+/**
+ * Ниже представлен метод calculate(int num1, int num2), который принимает 2 целых положительных числа.
+ *
+ * Задание: Написать код, который выводит в консоль разницу между первым и вторым числом, если первое больше второго, разницу между вторым и первым числом, если второе больше первого или произведение этих чисел, если они равны.
+ *
+ * Например, для чисел 2 и 5 вывод будет:
+ *
+ * 3
+ *
+ * @author Ilya Kaltygin
+ */
+public class Task32 {
+    public static void calculate(int num1, int num2) {
+        if (num1 > num2) {
+            System.out.println(num1 - num2);
+        } else if (num2 > num1) {
+            System.out.println(num2 - num1);
+        } else {
+            System.out.println(num1 * num2);
+        }
+    }
+}
