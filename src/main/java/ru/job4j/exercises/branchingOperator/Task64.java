67ca0e93c700003d1f4aea675a3125d7fad34aaf
Ilya <i.kaltygin@icloud.com>
2023-01-27 21:43:26 +0700

    64. Вывести текстовую форму оценки

---
+++ src/main/java/ru/job4j/exercises/branchingOperator/Task64.java
@@ -0,0 +1,24 @@
+package ru.job4j.exercises.branchingOperator;
+
+/**
+ * Ниже представлен метод gradePrint(int number), который принимает целое число.
+ *
+ * Задание: Написать код, который выводит в консоль текстовую форму оценки: 1-2 - "Плохо", 3 - "Удовлетворительно", 4 - "Хорошо", 5 - "Отлично". Если число не соответствует какой-либо оценке - вывести "Не соответствует".
+ *
+ * Например, для числа 5 вывод будет:
+ *
+ * Отлично
+ *
+ * @author Ilya Kaltygin
+ */
+public class Task64 {
+    public static void gradePrint(int number) {
+        switch (number) {
+            case 1, 2-> System.out.println("Плохо");
+            case 3 -> System.out.println("Удовлетворительно");
+            case 4 -> System.out.println("Хорошо");
+            case 5 -> System.out.println("Отлично");
+            default -> System.out.println("Не соответствует");
+        }
+    }
+}
