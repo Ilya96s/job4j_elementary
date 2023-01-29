6f50dd4073a2a14226b0b19d4d8294dc7b5609a5
Ilya <i.kaltygin@icloud.com>
2023-01-29 23:31:25 +0700

    80. Идентичны ли символьные значения

---
+++ src/main/java/ru/job4j/exercises/branchingOperator/Task80.java
@@ -0,0 +1,14 @@
+package ru.job4j.exercises.branchingOperator;
+
+/**
+ * Ниже представлен метод isIdentical(char s1, shar s2), который принимает 2 символьных значения.
+ *
+ * Задание: Написать код, который проверяет идентичность этих значений.
+ *
+ * @author Ilya Kaltygin
+ */
+public class Task80 {
+    public static boolean isIdentical(char s1, char s2) {
+        return s1 == s2;
+    }
+}
