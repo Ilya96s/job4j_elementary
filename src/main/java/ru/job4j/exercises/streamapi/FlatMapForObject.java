d7868f5d36b8cf3723dfa3e23656f8e12e82334c
Ilya <i.kaltygin@icloud.com>
2023-05-21 00:02:27 +0600

    6.7. Промежуточные операции. Метод flatMap()

---
+++ src/main/java/ru/job4j/exercises/streamapi/FlatMapForObject.java
@@ -0,0 +1,16 @@
+package ru.job4j.exercises.streamapi;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Ilya Kaltygin
+ */
+public class FlatMapForObject {
+    public static List<Integer> flatMap(List<List<Integer>> lists) {
+        return lists.stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
+}
