862ea3942e84916bfbbdf70ae06de2d8e4ca2e84
Ilya <i.kaltygin@icloud.com>
2023-06-21 19:31:28 +0700

    2.1. Способы сортировки. Сортировка массива примитивов

---
+++ src/main/java/ru/job4j/exercises/sort/ArraysSort.java
@@ -0,0 +1,13 @@
+package ru.job4j.exercises.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author Ilya KAltygin
+ */
+public class ArraysSort {
+    public static long[] sort(long[] data) {
+        Arrays.sort(data);
+        return data;
+    }
+}
