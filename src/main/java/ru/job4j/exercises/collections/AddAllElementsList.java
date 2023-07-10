60b50d2dba3e844e901e6720f01377911f78e342
Ilya <i.kaltygin@icloud.com>
2023-07-10 21:50:28 +0700

    7. Добавление списка в другой список

---
+++ src/main/java/ru/job4j/exercises/collections/AddAllElementsList.java
@@ -0,0 +1,18 @@
+package ru.job4j.exercises.collections;
+
+import java.util.List;
+
+/**
+ * метод принимает 2 списка и элемент, который возможно содержится в этих списках. Вам необходимо проверить оба списка, содержат ли они одновременно этот элемент, если содержит - необходимо удалить элемент из первого списка. Потом второй список нужно добавить в первый. Метод должен вернуть индекс элемента, который пришел в параметрах метода из общего списка.
+ *
+ * @author Ilya Kaltygin
+ */
+public class AddAllElementsList {
+    public static int containsElement(List<String> left, List<String> right, String str) {
+        if (left.contains(str) && right.contains(str)) {
+            left.remove(str);
+        }
+        left.addAll(right);
+        return left.indexOf(str);
+    }
+}
