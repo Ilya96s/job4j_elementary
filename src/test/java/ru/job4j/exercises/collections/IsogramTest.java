02eedc53acd1a37d54da4d954413c2fd1fd02854
Ilya96s <i.kaltygin@icloud.com>
2023-08-23 21:17:09 +0600

    6. Изограмма

---
+++ src/test/java/ru/job4j/exercises/collections/IsogramTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.exercises.collections;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class IsogramTest extends TestCase {
+
+    @Test
+    public void checkStringTrue() {
+        String s = "uncopyrightables";
+        boolean b = Isogram.checkString(s);
+        assertThat(b, is(true));
+    }
+
+    @Test
+    public void checkStringFalse() {
+        String s = "javascript";
+        boolean b = Isogram.checkString(s);
+        assertThat(b, is(false));
+    }
+
+}
