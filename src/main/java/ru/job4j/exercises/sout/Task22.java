7df2a485359b172e26936e75f077ee58882ecaa5
Ilya96s <i.kaltygin@icloud.com>
2023-09-04 22:27:10 +0600

    22. Форматированный вывод

---
+++ src/main/java/ru/job4j/exercises/sout/Task22.java
@@ -0,0 +1,7 @@
+package ru.job4j.exercises.sout;
+
+public class Task22 {
+    public static void nameInsert(String name) {
+        System.out.println("Привет, %s!".formatted(name));
+    }
+}
---
+++ src/test/java/ru/job4j/exercises/sout/Task22Test.java
@@ -0,0 +1,42 @@
+package ru.job4j.exercises.sout;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class Task22Test {
+
+    public String ln = System.lineSeparator();
+
+    @Test
+    public void whenPetrThenHiPetr() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task22.nameInsert("Петр");
+        String expected = "Привет, Петр!" + ln;
+        assertThat(out.toString(), is(expected));
+    }
+
+    @Test
+    public void whenIvanThenHiIvan() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task22.nameInsert("Иван");
+        String expected = "Привет, Иван!" + ln;
+        assertThat(out.toString(), is(expected));
+    }
+
+    @Test
+    public void whenElenThenHiElena() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task22.nameInsert("Елена");
+        String expected = "Привет, Елена!" + ln;
+        assertThat(out.toString(), is(expected));
+    }
+
+}
